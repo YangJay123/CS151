8c85d3b7a4b16310dba97e2c86b5cfe8f6c9dc13
llege <llege@10.0.0.157>
2024-12-02 16:52:36 -0800

    Added most get and set methods to customers
    Note: getDetail() method has been implemented

--- src/hw/Customer.java
+++ src/hw/Customer.java
@@ -3,6 +3,8 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+import hw.CustomExceptions.ItemNotAvailableException;
+
 public class Customer implements User, Serializable {
     private static final long serialVersionUID = 1L;
 
@@ -24,5 +26,85 @@ public class Customer implements User, Serializable {
         this.isActive = isActive;
         this.orderedItems = new ArrayList<>();
     }
+    
 	// xxx your codes
+    public String getFirstName() {
+    	return this.firstName;
+    }
+    
+    public String getLastName() {
+    	return this.lastName;
+    }
+    
+    public String getEmail() {
+    	return this.email;
+    }
+    
+    public String getUserName() {
+    	return this.userName;
+    }
+    
+    public String getPassword() {
+    	return this.password;
+    }
+    
+    public void setUserName(String s) {
+    	this.userName = s;
+    }
+    
+    public boolean isActive() {
+    	return this.isActive;
+    }
+    
+    public void setActive(boolean b) {
+    	this.isActive = b;
+    }
+
+	
+	@Override
+	public List<String> getOrderedItems() {
+		return this.orderedItems;
+	}
+
+	@Override
+	public String getRole() {
+		return "customer";
+	}
+
+	@Override
+	public void orderItems(MenuItem item) throws ItemNotAvailableException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setOrderedItems(List<String> orderedItems) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void cancelItem(MenuItem item) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean canPlace() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String getDetails() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String toDataString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+    
 }
