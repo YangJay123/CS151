67d5b1763a6e5e64d5ccbc22fcd8f5409a71167e
llege <llege@10.0.0.157>
2024-12-02 18:28:40 -0800

    Added get and set methods for admin class Note: detail method not implemented

--- src/hw/Admin.java
+++ src/hw/Admin.java
@@ -3,6 +3,8 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+import hw.CustomExceptions.ItemNotAvailableException;
+
 public class Admin implements User, Serializable {
 	private static final long serialVersionUID = 1L;
 	
@@ -25,7 +27,102 @@ public class Admin implements User, Serializable {
         this.isActive = isActive;
         this.orderedItems = new ArrayList<>();
     }
+
+	@Override
+	public int compareTo(User o) {
+		return this.userName.compareTo(o.getUserName());
+	}
+
+	@Override
+	public String getFirstName() {
+		return this.firstName;
+	}
+
+	@Override
+	public String getLastName() {
+		return lastName;
+	}
+
+	@Override
+	public String getEmail() {
+		return email;
+	}
+
+	@Override
+	public String getUserName() {
+		return userName;
+	}
+
+	@Override
+	public String getPassword() {
+		return password;
+	}
+
+	@Override
+	public boolean isActive() {
+		return isActive;
+	}
+
+	@Override
+	public List<String> getOrderedItems() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getRole() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void orderItems(MenuItem item) throws ItemNotAvailableException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setActive(boolean active) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setOrderedItems(List<String> orderedItems) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setUserName(String userName) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void cancelItem(MenuItem item) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean canPlace() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String getDetails() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String toDataString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
     
    // xxx your codes
-    //test
+
 }
