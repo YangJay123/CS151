0218d04b5c589a37ff537140a31612eb28268c57
llege <llege@10.0.0.157>
2024-12-03 19:01:47 -0800

    changed button size in default frame

--- src/hw/CafeOnlineOrderSystemGUI.java
+++ src/hw/CafeOnlineOrderSystemGUI.java
@@ -11,32 +11,42 @@ public class CafeOnlineOrderSystemGUI extends JFrame{
 	
 	public CafeOnlineOrderSystemGUI() {
         // Set up the frame
-        setTitle("Pug's Puncake Diner Co.");
-        setSize(800, 450);
-        setLocationRelativeTo(null);
-        setLayout(new BorderLayout());
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JFrame frame = new JFrame("Dineer");
+		frame.setTitle("Pug's Puncake Diner Co.");
+		frame.setSize(800, 450);
+		frame.setLocationRelativeTo(null);
+		frame.setLayout(new GridBagLayout());
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
+		GridBagConstraints gbc = new GridBagConstraints();
+		// xxx specify that the component's display area will be from gridx to 
+		// xxx the last cell in the row
+        gbc.gridwidth = GridBagConstraints.REMAINDER;
+		// xxx Make the component wide enough to fill its display area horizontally, 
+		// xxx but do not change its height.
+        gbc.fill =      GridBagConstraints.HORIZONTAL;
+        gbc.insets = new Insets(10, 50, 10, 50); 
+		
    		// Welcome title
         JLabel welcomeLabel = new JLabel("Welcome to Pug's Puncake Diner!", SwingConstants.CENTER);
-        welcomeLabel.setFont(new Font("Serif", Font.BOLD, 24));
-        add(welcomeLabel, BorderLayout.CENTER);
+        welcomeLabel.setFont(new Font("Serif", Font.BOLD, 32));
+      
         
-        JPanel buttonPanel = new JPanel(new GridLayout(4,1,10,10));
         
         JButton loginButton = new JButton("Login");
         JButton exitButton = new JButton("Exit");
         exitButton.addActionListener(e -> System.exit(0));
         JButton signUpButton = new JButton("Sign up");
-        
-        buttonPanel.add(loginButton);
-        buttonPanel.add(exitButton);
+       
         
         JLabel notUser = new JLabel("Not a User? Click here to sign up!", SwingConstants.CENTER);
         notUser.setFont(new Font("Sans", Font.PLAIN, 16));
         
-        buttonPanel.add(notUser);
-        buttonPanel.add(signUpButton);
+        frame.add(welcomeLabel, gbc);
+        frame.add(loginButton, gbc);
+        frame.add(exitButton,gbc);
+        frame.add(notUser, gbc);
+        frame.add(signUpButton, gbc);
         
         loginButton.addActionListener(e -> {
             LoginScreen loginScreen = new LoginScreen(CafeOnlineOrderSystemGUI.this, null);
@@ -47,9 +57,7 @@ public class CafeOnlineOrderSystemGUI extends JFrame{
             signUp.setVisible(true);
         });
         
-        add(buttonPanel, BorderLayout.SOUTH);
-        
-        
+        frame.setVisible(true);
 	}
 	public static void main(String[] args) {
 		 SwingUtilities.invokeLater(() -> {
